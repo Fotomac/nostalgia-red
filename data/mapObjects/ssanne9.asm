a9e529d59bc1e91d35bb54b1bf20bba9ef5c37c5
Fotomac <ryanasaurus0077@gmail.com>
2017-06-30 13:40:52 -0400

    First Lass on Route 25 is now a Picnicker

--- data/mapObjects/ssanne9.asm
+++ data/mapObjects/ssanne9.asm
@@ -21,7 +21,7 @@ SSAnne9Object:
 	object SPRITE_GENTLEMAN, $a, $2, STAY, RIGHT, $1, OPP_GENTLEMAN, $3
 	object SPRITE_FISHER2, $d, $4, STAY, LEFT, $2, OPP_FISHER, $1
 	object SPRITE_GENTLEMAN, $0, $e, STAY, RIGHT, $3, OPP_GENTLEMAN, $5
-	object SPRITE_LASS, $2, $b, STAY, DOWN, $4, OPP_LASS, $c
+	object SPRITE_LASS, $2, $b, STAY, DOWN, $4, OPP_LASS, $b
 	object SPRITE_GENTLEMAN, $1, $2, STAY, DOWN, $5 ; person
 	object SPRITE_BALL, $c, $1, STAY, NONE, $6, MAX_ETHER
 	object SPRITE_GENTLEMAN, $15, $2, STAY, DOWN, $7 ; person
